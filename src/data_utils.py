1743a6cced90e857f8e070867c0c9fd2a4f8b45c
Mohamed Naas <m.naas@esi-sba.dz>
2022-06-20 15:08:00 +0200

    download dataset using hugging faces

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,6 +1,6 @@
 from typing import Optional
 import pytorch_lightning as pl
-from torch.utils.data import random_split, DataLoader
+from torch.utils.data import DataLoader
 from torchvision import transforms as T
 from datasets import load_dataset
 
@@ -9,31 +9,35 @@ class WiderFaceDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str = "./"):
         super().__init__()
         self.data_dir = data_dir
-        self.transform = T.Compose([T.ToTensor()])
+        self.transform = T.Compose([ T.Resize((256,256)), T.ToTensor()])
         # maybe also normelize
-
-    def setup(self, stage: Optional[str] = None):
-        # download the dataset
-        # to do: only load train and validation sets
-        self.dataset = load_dataset("wider_face", data_dir=self.data_dir)
-        # Assign train/val datasets for use in dataloaders
-        self.train_set = self.dataset["train"]
-        self.train_set.with_transform(self.transforms)
-        self.val_set = self.dataset["validation"]
-        self.val_set.with_transform(self.transforms)
-
 
     def prepare_data(self):
         load_dataset("wider_face", data_dir=self.data_dir)
-        
-    def transforms(self, data):
-        #  [self.transform(image["image"]) for image in data]
-        new_data = []
-        for example in data:
-            example["image"] = self.transform(example["image"])
-            new_data.append(example)
-        return new_data
 
+    def setup(self, stage: Optional[str] = None):
+        self.dataset = load_dataset("wider_face")
+        
+
+        self.dataset.set_transform(self.pil_image_transform, columns=["image","faces"], output_all_columns=True)
+        # max = 0
+        # for i in range(len(self.dataset)):
+        #     if len(self.dataset["train"]["faces"][i]) > max:
+        #         max = len(self.dataset["train"]["faces"][i])
+        # print(max)
+        # self.dataset.set_transform(self.labels_transform, columns="faces")
+        self.train_set = self.dataset["train"]
+        self.val_set = self.dataset["validation"]
+
+
+
+    def pil_image_transform(self, batch):
+        return {"image": [self.transform(img) for img in batch["image"]],
+        "faces": [label["bbox"][0] for label in batch["faces"]]
+        } 
+
+    def labels_transform(self, batch):
+        return {"faces": [label["bbox"][0] for label in batch["faces"]]}
     def train_dataloader(self):
         return DataLoader(self.train_set, batch_size=32)
 
@@ -44,4 +48,5 @@ class WiderFaceDataModule(pl.LightningDataModule):
 dm = WiderFaceDataModule()
 dm.prepare_data()
 dm.setup()
+print(dm.train_set.__getitem__(2))
 print(next(iter(dm.train_dataloader())))
