aa024710064f69abcad760bc438d110df13076fb
Mohamed Naas <m.naas@esi-sba.dz>
2022-07-20 17:48:32 +0200

    add convert to onnx feature in train

--- deploy/inference.py
+++ deploy/inference.py
@@ -6,9 +6,9 @@ import time
 import torch
 from collections import OrderedDict
 import argparse
+from retinaface.pre_trained_models import get_model
 
-
-def get_model(model_path, max_size=512, device="cpu"):
+def get_model_local(model_path, max_size=512, device="cpu"):
     model = Model(max_size=max_size, device=device)
     state_dict = torch.load(model_path, map_location=device)
     new_state_dict = OrderedDict()
@@ -16,16 +16,15 @@ def get_model(model_path, max_size=512, device="cpu"):
         name = k[6:]  # remove `module.`
         new_state_dict[name] = v
     model.load_state_dict(new_state_dict)
-    model.eval()
     return model
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg("-i", "--image_path", help="Path of the input image.", required=True)
+    arg("-l", "--local_model", help="if the model is local", action='store_true')
     arg("-o", "--output_path", help="Path of the outputed image.", required=True)
-    arg("-m", "--model_path", help="Path of the model.", required=True)
+    arg("-m", "--model_path", help="Path of the model.")
     arg(
         "-ms",
         "--max_size",
@@ -41,12 +40,17 @@ if __name__ == "__main__":
         default="cpu",
     )
     args = parser.parse_args()
-    model = get_model(args.model_path, args.max_size, args.device)
     image = cv2.imread(args.image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    start = time.time()
-    annotation = model.predict_jsons(image)
-    print(f"inference done in {time.time() - start:0.3f} secs")
+    if args.local_model:
+        model = get_model_local(args.model_path, args.max_size, args.device)
+    else:
+        model = get_model("resnet50_2020-07-20", args.max_size, args.device)
+    model.eval()
+    with torch.no_grad():
+        start = time.time()
+        annotation = model.predict_jsons(image)
+        print(f"inference done in {time.time() - start:0.3f} secs")
 
     plt.imsave(args.output_path, vis_annotations(image, annotation))
